614c02ad3b03c1c382c1de32ddbdd3d00cf6aac0
Arjun Mayilvaganan <arjunmayilvaganan@gmail.com>
2014-12-29 22:38:56 +0530

    NSTEPS

---
+++ 1112.cpp
@@ -0,0 +1,25 @@
+//1112
+//Number Steps
+#include<iostream>
+using namespace std;
+int main()
+{
+	int t,x,y;
+	scanf("%d",&t);
+	while(t--)
+	{
+		scanf("%d%d",&x,&y);
+		if((x==y)||(y==x-2))
+		{
+			if(x%2==0&&y%2==0)
+				printf("%d\n",x+y);
+			else if(x%2!=0&&y%2!=0)
+				printf("%d\n",x+y-1);
+			else
+				printf("No Number\n");
+		}
+		else
+			printf("No Number\n");
+	}
+	return 0;
+}
