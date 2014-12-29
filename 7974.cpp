256c43fcdc9fbc2c9e41261f95e2b80a3dab810b
Arjun Mayilvaganan <arjunmayilvaganan@gmail.com>
2014-12-30 00:22:20 +0530

    ACPC10A

---
+++ 7974.cpp
@@ -0,0 +1,19 @@
+//7974
+//ACPC10A
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	while(1)
+	{
+		cin>>a>>b>>c;
+		if(a==0&&b==0&&c==0)
+			break;
+		if((c-b==b-a)&&(b-a)!=0)
+			cout<<"AP "<<(c+(b-a))<<endl;
+		else
+			cout<<"GP "<<(c*(b/a))<<endl;
+	}
+	return 0;
+}
